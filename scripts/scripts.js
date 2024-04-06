d84ca74865acdf1b8f28b9ffb13c56ae55b759f9
jago-1009 <garwoodj22@gmail.com>
2024-04-06 19:35:52 -0400

    testing for mobile nav.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,12 +3,8 @@ function toggleMenu() {
    var x = document.getElementsByClassName("links")
    var icon = document.getElementById("icon")
     for (i=0;i<x.length;i++) {
-        if (document.getElementsByClassName("links")[i].style.display == "inline") {
-            document.getElementsByClassName("links")[i].style.display = "none"
-        }
-        else {
-        document.getElementsByClassName("links")[i].style.display = "inline"
-        }
+        x[i].classList.toggle("mobile")
+        
     }
     icon.classList.toggle('fa-bars')
     icon.classList.toggle('fa-x')
